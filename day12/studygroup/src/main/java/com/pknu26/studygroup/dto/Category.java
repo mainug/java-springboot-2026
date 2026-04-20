9902270476ecbee099ac9771a1d729a1d9a99b16
mainug <alsdnr0850@gmail.com>
2026-04-20 17:24:24 +0900

    spring 12일차

---
+++ day12/studygroup/src/main/java/com/pknu26/studygroup/dto/Category.java
@@ -0,0 +1,13 @@
+package com.pknu26.studygroup.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+}
